7ab7366c0b8cdc0a9bdb9cc12d53a285b76672f1
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-20 10:46:18 +0300

    show toast message on sign in + change favicon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { auth } from './firebase.config'
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
+import ChatToast from "./components/ChatToast";
+
 function App() {
+  //controller to navigate user to different page
   const navigateController = useNavigate();
 
   const logOutUser = async () => {
@@ -18,6 +21,8 @@ function App() {
 
   return (
     <div className="text-3xl font-bold underline">
+
+      <ChatToast toastMsg="Signed in successfully" />
 
       <button className="btn" onClick={logOutUser}>
         SignOut</button>
