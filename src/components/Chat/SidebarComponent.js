c0f47c475cf0362fc2d08e15dad92f5680836499
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-22 16:39:57 +0300

    add sidebarChatContainer

--- src/components/Chat/SidebarComponent.js
+++ src/components/Chat/SidebarComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SidebarHeader from "./SidebarComponents/SidebarHeader";
-import SidebarChat from "./SidebarComponents/SidebarChat";
+import SidebarChatContainer from "./SidebarComponents/SidebarCatContainer";
 
 
 const SidebarComponent = (props) => {
@@ -9,25 +9,7 @@ const SidebarComponent = (props) => {
         <div className="border-r border-r-gray flex flex-col bg-blue-300 text-slate-950 font-nunito relative h-screen overflow-auto">
             <SidebarHeader />
 
-            {/* Chats container */}
-            <div className="flex flex-col overflow-y-auto scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-slate-400 hover:scrollbar-thumb-slate-500 
-            hover:scrollbar-track-slate-300 scrollbar-thumb-rounded scrollbar-track-rounded ">
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-            </div>
+            <SidebarChatContainer />           
         </div>
     );
 
---
+++ src/components/Chat/SidebarComponents/SidebarCatContainer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import SidebarChat from "./SidebarChat";
+
+const SidebarChatContainer = () => {
+    return (
+        <div className="flex flex-col overflow-y-auto scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-slate-400 hover:scrollbar-thumb-slate-500 
+        hover:scrollbar-track-slate-300 scrollbar-thumb-rounded scrollbar-track-rounded">
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+        </div>
+    );
+}
+
+export default SidebarChatContainer;
