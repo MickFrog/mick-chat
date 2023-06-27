6aa3e97a789faaf6ee4a75cc58e134218b277651
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-28 00:56:10 +0300

    fetch chat messages on chat select

--- src/components/Chat/ChatAreaComponents/ChatsContainer.js
+++ src/components/Chat/ChatAreaComponents/ChatsContainer.js
@@ -1,14 +1,34 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import ReceivedMessage from "./ChatsContainerComponents/ReceivedMessage";
 import SentMessage from "./ChatsContainerComponents/SentMessage";
+import { doc, onSnapshot } from "firebase/firestore";
+import { fireDB } from "../../../firebase.config";
+import { ChatContext } from "../../../ChatContext";
 
 const ChatsContainer = () => {
     //reference to scroll automatically to bottom of messages whenever the messages change
     const messageEndRef = useRef(null);
+    const { data } = useContext(ChatContext);
+    const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        messageEndRef.current?.scrollIntoView();
-    }, []) //messages array dependency supposed to be here
+    useEffect(() => { //load active chat (chatId from state in Chat Context) messages 
+        if(data.chatId === '') return; //prevent empty id before choosing chat
+
+        const fetchMessages = () => {
+            const unsub = onSnapshot(doc(fireDB, "chats", data.chatId), (doc) => {
+                //if doc exists then set messages
+                doc.exists() && setMessages(doc.data().message);
+            });
+
+            return () => unsub();
+        }
+
+        fetchMessages()
+    }, [data.chatId])
+
+    useEffect(() => { //scroll to bottom of messages on new messages arrival
+        messageEndRef.current?.scrollIntoView({ behavior: "smooth"});
+    }, [messages])
 
     return (
         <div className="h-[--chatsContainerHeight] px-4 overflow-auto my-custom-scrollbar 
