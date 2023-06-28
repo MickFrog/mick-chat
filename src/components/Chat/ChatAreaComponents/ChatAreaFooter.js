cd7daead3570edde9ffd243dae7031c8d666f66e
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-28 16:13:06 +0300

    add send msgs to db functionality+update last msg

--- src/components/Chat/ChatAreaComponents/ChatAreaFooter.js
+++ src/components/Chat/ChatAreaComponents/ChatAreaFooter.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Tooltip } from "flowbite-react";
 import sendImg from '../../../images/sendIcon.svg'
+import { Timestamp, arrayUnion, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { fireDB } from "../../../firebase.config";
+import { ChatContext } from "../../../ChatContext";
+import uniqid from 'uniqid';
+import { AppContext } from "../../../App";
 
 const ChatAreaFooter = () => {
+    const [newMsg, setNewMsg] = useState("");
+
+    const { currentUser } = useContext(AppContext);
+    const { data } = useContext(ChatContext);
+
+    const handleSendMessage = async () => {
+        //prevent sending empty msg + sending message when no chat selected
+        if(newMsg === "" || data.userInfo === {}) return; 
+
+        try {
+            //update messages array btn user chatting in chats collection
+            await updateDoc(doc(fireDB, "chats", data.chatId), {
+                messages: arrayUnion({
+                    msgId: uniqid(),
+                    senderId: currentUser.uid,
+                    msgDate: Timestamp.now(),
+                    msgText: newMsg
+                })
+            });
+    
+            //update lastMessage + date for currentUser + other user chatting in userChats collection
+            await updateDoc(doc(fireDB, "userChats", currentUser.uid), {
+                [data.chatId + ".date"]: serverTimestamp(),
+    
+                [data.chatId + ".lastMessage"]: {
+                    lastMessageText: newMsg,
+                }
+            });
+    
+            await updateDoc(doc(fireDB, "userChats", data.userInfo.userId), {
+                [data.chatId + ".date"]: serverTimestamp(),
+                
+                [data.chatId + ".lastMessage"]: {
+                    lastMessageText: newMsg,
+                }
+            });
+            
+            setNewMsg("");
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div className="h-[75px] bg-blue-300 p-4 flex gap-2 justify-around items-center light-blue-dark">
             <textarea className=" w-[90%] rounded bg-blue-200 dark:bg-slate-700 border-0 drop-shadow-sm resize-none my-custom-scrollbar 
                 hover:bg-blue-100 dark:hover:bg-slate-600 focus:bg-blue-100 dark:focus:bg-slate-600"
-                placeholder="Enter your message here...">
+                placeholder="Enter your message here..." onChange={(e) => setNewMsg(e.target.value)} value={newMsg}>
 
             </textarea>
 
             <Tooltip content="Send Message" placement="top">
-                <button className="py-1 px-1 rounded shadow text-xl btn-white btn-white:hover">
+                <button className="py-1 px-1 rounded shadow text-xl btn-white btn-white:hover" onClick={handleSendMessage}>
                     <img className="w-8 h-8 rounded" src={sendImg} alt="Toggle theme"/>
                 </button>
             </Tooltip>
--- src/components/Chat/SidebarComponents/SidebarChat.js
+++ src/components/Chat/SidebarComponents/SidebarChat.js
@@ -4,13 +4,13 @@ import { ChatContext } from "../../../ChatContext";
 
 const SidebarChat = (props) => {
     const { userInfo } = props.chatInfo;
+    const { lastMessageText } = props.chatInfo.lastMessage;
     const { dispatch } = useContext(ChatContext);
 
     const handleChatSelect = () => { //change state of active user in Chat context
         dispatch({type: "CHANGE_ACTIVE_CHAT", payload: userInfo})
     };
 
-    const tempLastMsg = "Yo Kalooli, I meant to check on you yesterday but sth unfortunate happened!";
     return (
         <div className="bg-blue-400 dark:bg-slate-800 hover:bg-blue-500 dark:hover:bg-gray-600 ease-in-out duration-200 flex items-center 
             gap-2 px-1 py-2 border-b border-b-gray cursor-pointer" onClick={handleChatSelect}>
@@ -22,7 +22,13 @@ const SidebarChat = (props) => {
 
             <div>
                 <p className="text-xl font-semibold">{userInfo.displayName}</p>
-                <p className="text-base">{tempLastMsg.substring(0, 48)}...</p>
+                <p className="text-base">
+                    {   
+                        lastMessageText.length < 48 
+                        ? lastMessageText 
+                        : lastMessageText.substring(0, 48) + "..."
+                    }
+                </p>
             </div>
         </div>
     );
